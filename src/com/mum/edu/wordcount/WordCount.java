a361e45fe8921bd422d968d86431652cb18833d9
cloudera <cloudera@quickstart.cloudera>
2018-01-30 09:38:52 -0800

    Initial commit

---
+++ src/com/mum/edu/wordcount/WordCount.java
@@ -0,0 +1,80 @@
+package com.mum.edu.wordcount;
+        
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+        
+public class WordCount {
+        
+ public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private HashMap<Text, IntWritable> mapper;
+    
+    protected void setup(Context context)throws IOException, InterruptedException{
+    	mapper = new HashMap<>();
+    }
+        
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+            Text word = new Text();
+            word.set(tokenizer.nextToken());
+            if(mapper.containsKey(word)){
+            	IntWritable times = new IntWritable(mapper.get(word).get()+1);
+            	mapper.put(word, times);
+            }else{
+            	mapper.put(word, new IntWritable(1));
+            }            
+        }
+    }
+    
+    protected void cleanup(Context context)throws IOException, InterruptedException {
+    	
+    	for(Text word: mapper.keySet()){
+    		context.write(word, mapper.get(word));
+    	}
+    }
+ } 
+        
+ public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+      throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        context.write(key, new IntWritable(sum));
+    }
+ }
+        
+ public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+        
+        Job job = new Job(conf, "wordcount");
+        job.setJarByClass(com.mum.edu.wordcount.WordCount.class);
+    
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+        
+    job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce.class);
+        
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+        
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+    job.waitForCompletion(true);
+ }
+        
+}
